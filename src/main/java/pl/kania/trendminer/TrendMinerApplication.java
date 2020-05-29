452fd57b99dcbd1c72f276e09c8f29fc6a7a97bf
jaskier07 <aleksanderkania96@gmail.com>
2020-05-29 15:08:48 +0200

    Initial Spring commit

---
+++ src/main/java/pl/kania/trendminer/TrendMinerApplication.java
@@ -0,0 +1,13 @@
+package pl.kania.trendminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendMinerApplication.class, args);
+	}
+
+}
